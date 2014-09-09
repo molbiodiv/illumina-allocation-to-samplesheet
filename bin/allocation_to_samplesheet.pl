fefe5c17646587814ce1b142f1104192e37dd628
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2014-09-09 10:18:49 +0200

    added first quick and dirty script for the allocation to samplesheet conversion

---
+++ bin/allocation_to_samplesheet.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+# USAGE: perl allocation_to_samplesheet.pl <allocation.tsv> <barcode.tsv> [header.tsv] >samplesheet.tsv
+
+$Data::Dumper::Sortkeys = 1;
+
+my @letters = ("A","B","C","D","E","F","G","H");
+my $col = 1;
+my $row = 1;
+my $plate = 0;
+my %sample;
+my @forward;
+my %seqs;
+
+open IN, "<$ARGV[1]" or die "$!";
+while(<IN>){
+    chomp;
+    my ($sid, $seq) = split(/\t/);
+    $seqs{$sid} = $seq;
+}
+close IN or die "$!";
+
+if($ARGV[2]){
+open IN, "<$ARGV[2]" or die "$!";
+while(<IN>){
+    print;
+}
+close IN or die "$!";
+
+}
+
+open IN, "<$ARGV[0]" or die "$!";
+while(<IN>){
+    chop;
+    my @row = split(/\t/);
+    # Not a header row (first and tenth cell contain reverse primers)
+    if($row[0]){
+	my $rev = $row[0];
+	my $col = 1;
+	my $plateadd = 0;
+	for(my $c=1; $c<18; $c++){
+	    if($c==9){
+		$rev = $row[$c];
+		$col = 1;
+		$plate += 2;
+	    }
+	    else{
+		$row[$c] = "" unless(defined $row[$c]);
+		$sample{$plate}{$letters[$col-1]}{$row} = [$row[$c], $forward[$c], $rev];
+		$col++;
+	    }
+	}
+	$row++;
+	$plate -= 2;
+    }
+    # Header row (contains forward primers as colnames)
+    else{
+	@forward = @row;
+	$row = 1;
+	$plate++;
+    }
+}
+close IN or die "$!";
+
+for(my $p=1; $p<5; $p++){
+    foreach my $r (sort keys %{$sample{$p}}){
+	foreach my $c (sort {$a <=> $b} keys %{$sample{$p}{$r}}){
+	    my ($id, $f, $re) = @{$sample{$p}{$r}{$c}};
+	    print "$id\t$id\t$p\t$c$r\t$f\t$seqs{$f}\t$re\t$seqs{$re}\tproject\tdescription\n";
+	}
+    }
+}
+
+# print Dumper(\%sample);
